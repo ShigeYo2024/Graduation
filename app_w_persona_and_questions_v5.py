b1e38dd0db033bf8e20d6b7689d07c6019503855
ShigeYo2024 <syai0124@gmail.com>
2025-02-14 01:29:00 +0900

    Update app_w_persona_and_questions_v5.py

--- app_w_persona_and_questions_v5.py
+++ app_w_persona_and_questions_v5.py
@@ -2,6 +2,8 @@ import streamlit as st
 import openai
 import json
 import os
+import pandas as pd
+import io
 
 # StreamlitのSecretsからOpenAI API keyを取得
 openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
